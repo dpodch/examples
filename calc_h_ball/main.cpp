9e068fd7a64933f8f6186a64e8952225490abda0
Денис <dpdoch@gmail.com>
2020-02-12 00:42:55 +0300

    save chages

--- calc_h_ball/main.cpp
+++ calc_h_ball/main.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 
-float calcHeight(float timeInSec, float initialHeight)
+float calcHeight(float timeInMs, float initialHeight)
 {
 	static const float g = 9.8;
-	return initialHeight - g * pow(timeInSec, 2) / 2;
+	return initialHeight - g * pow(timeInMs / 1000, 2) / 2;
 }
 
 float getHeight()
@@ -18,19 +18,58 @@ float getHeight()
 	return height;
 }
 
+float getNextTimeMs(float timeMs, float stepMs, float initialHeight, float &newHeight)
+{
+	float newTimeMs = timeMs + stepMs;
+
+	newHeight = calcHeight(newTimeMs, initialHeight);
+
+	if (newHeight < 0)
+	{
+		float height = calcHeight(timeMs, initialHeight);
+		float k = height / (height - newHeight);
+
+		newHeight = 0;
+		newTimeMs = timeMs + stepMs * k;
+	}
+	return newTimeMs;
+}
+
+void print(float timeMs, float height)
+{
+	int sec = timeMs / 1000;
+	int ms = timeMs - (sec * 1000);
+	cout << "At";
+	if (sec > 0)
+	{
+		cout << " " << sec << " sec";
+	}
+
+	if (ms > 0)
+	{
+		cout << " " << ms << " ms";
+	}
+	cout <<", the ball is at the height: " << height << " meteres" << endl;
+}
+
 int main() 
 {
 	const float initialHeight = getHeight();
-	float height = initialHeight;
-
-	cout << "the ball is at the height: " << height << "meteres" << endl;
 	
-	int timeInSec = 0;
-	while (height > 0)
+	float timeInMs = 0;
+	float timeStepMs = 1000;
+	while (initialHeight >= 0)
 	{
-		timeInSec++;
-		height = calcHeight(timeInSec, initialHeight);
-		cout << "At " << timeInSec << "seconds, the ball is at the height: " << height << "meteres" << endl;
+		float newHeight = 0;
+		float newTimeMs = getNextTimeMs(timeInMs, timeStepMs, initialHeight, newHeight);
+		print(newTimeMs, newHeight);
+
+		if (newHeight <= 0)
+		{
+			break;
+		}
+
+		timeInMs = newTimeMs;
 	}
 
 	return 0;
